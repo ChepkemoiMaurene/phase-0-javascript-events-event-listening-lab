ffae61e64c81d7a7135c486d7cf003796ae23e06
ChepkemoiMaurene <baurene.chepkemoi@student.moringaschool.com>
2022-08-26 13:20:07 +0300

    completed assignments

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-function addingEventListener() {
+// const element = document.querySelector("#main");
+// document.createElement(div)
+// element.addEventListener("click", (div) => {
+// 	console.log("add div");
+// });
+
+function addingEventListener(){
+	document.getElementById("input").addEventListener("input", addingEventListener);
+
+
 }
